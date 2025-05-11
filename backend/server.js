18d7c2048272f880fb42e33f3c8a8bdb8904d01f
bizualem <ansifabebe971@gmail.com>
2025-05-11 15:15:53 +0100

    connection establishment in moongose

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,19 @@
 //const express = require('express')
 
 import express from 'express';
-
+import dotenv from "dotenv";
+import { connectDB } from './config/db.js';
+dotenv.config();
 const app = express();
 
-app.get("/", (req, res) => {
-    res.send("server is ready");
-});
+
+app.get("/products", (req, res) => {});
+
 app.listen(5000, () => {
+    connectDB();
     console.log('server started at https://localhost:5000 ');
 
 });
-
-
-
-
 
 
 
