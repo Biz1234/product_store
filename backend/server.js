b7b71cb4d780db071005929752e2fc19bba89dcd
bizualem <ansifabebe971@gmail.com>
2025-05-12 10:00:39 +0100

    creating product sample

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,25 @@ dotenv.config();
 const app = express();
 
 
-app.get("/products", (req, res) => {});
+app.get("/products", async (req, res) => {
+
+    const product = req.body;
+
+if(!product.name || !product.price || !product.image) {
+    return res.status(400).json({success:false, message: "please provide all field"});
+}
+const newProduct = new Product(product)
+try {
+    await newProduct.save();
+    res.status(201).json({success:true,data: newProduct});
+
+} catch(error){
+    console.error("error in create product:", error.message);
+    res.status(500).json({success:false, message: "server error"});
+    
+}
+
+});
 
 app.listen(5000, () => {
     connectDB();
